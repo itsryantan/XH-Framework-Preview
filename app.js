6b60cf7806bec86bc2307e8920f004c005e079ca
Ryan Tan <itsryantan@yahoo.com>
2026-05-22 11:07:59 +0800

    Bump preview asset versions

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const guideBubbles = {
   unselected: "./assets/guide-unselected.png",
   selected: "./assets/guide-selected-mom.png",
   tempFromSelected: "./assets/guide-temp-from-selected.png",
-  selfFromUnselected: "./assets/guide-self-from-unselected.png?v=20260522-unselected-tab-guide",
-  momFromUnselected: "./assets/guide-mom-from-unselected.png?v=20260522-unselected-tab-guide"
+  selfFromUnselected: "./assets/guide-self-from-unselected.png?v=20260522-unselected-tab-guide-v2",
+  momFromUnselected: "./assets/guide-mom-from-unselected.png?v=20260522-unselected-tab-guide-v2"
 };
 
 const guideBubbleDataKeys = {
